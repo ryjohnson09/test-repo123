b600a3d438fc83781905ca0da2794fff6644102d
ryjohnson09 <johnsrc06@gmail.com>
2023-12-22 01:09:30 +0000

    Change app title

--- test-app/app.R
+++ test-app/app.R
@@ -26,7 +26,7 @@ means <- colMeans(
 # UI ----------------------------------------------------------------------
 
 ui <- page_sidebar(
-  title = "Penguins dashboard",
+  title = "Palmer Penguins dashboard",
   sidebar = sidebar(
     varSelectInput(
       "color_by", "Color by", 
